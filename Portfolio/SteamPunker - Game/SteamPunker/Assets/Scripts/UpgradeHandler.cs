b8d3171f271a0d7f9bb4afbe78975db7004cf2dd
nicklim011213 <113318249+nicklim011213@users.noreply.github.com>
2022-09-18 21:02:20 -0400

    Update UpgradeHandler.cs

--- Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/UpgradeHandler.cs
+++ Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/UpgradeHandler.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Unused / empty script
+ 
 public class UpgradeHandler : MonoBehaviour
 {
     // Start is called before the first frame update
