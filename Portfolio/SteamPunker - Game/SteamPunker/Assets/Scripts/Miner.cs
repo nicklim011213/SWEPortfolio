cd0573e9bc63e89b5f9a7c442c4b29101b8297d3
nicklim011213 <113318249+nicklim011213@users.noreply.github.com>
2022-09-18 20:48:42 -0400

    Update Miner.cs

--- Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Miner.cs
+++ Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Miner.cs
@@ -13,7 +13,7 @@ public class Miner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ResourceView = GameObject.Find("Canvas").transform.GetChild(2).GetComponent<Resource_Mgr>();
+        ResourceView = GameObject.Find("Canvas").transform.GetChild(2).GetComponent<Resource_Mgr>(); // Opens a simple screen displaying what it gathered
     }
 
     // Update is called once per frame
@@ -22,54 +22,54 @@ public class Miner : MonoBehaviour
 
         if (Input != null)
         {
-            if (Input.GetComponent<Steam>().steamheld > 2 || Input.GetComponent<Steam>().steamheld == 2)
+            if (Input.GetComponent<Steam>().steamheld > 2 || Input.GetComponent<Steam>().steamheld == 2) // If the input has > 2 steam do its efficency boost
             {
-                Chance = Random.Range(0.0f, 1.0f);
-                Input.GetComponent<Steam>().steamheld = Input.GetComponent<Steam>().steamheld - 2;
+                Chance = Random.Range(0.0f, 1.0f);                                                      // Determines resouce type
+                Input.GetComponent<Steam>().steamheld = Input.GetComponent<Steam>().steamheld - 2;      // remove 2 steam
                 Eff = true;
                 if (Chance >= .5 && CoalProgress < 100)
                 {
                     CoalProgress++;
                     CoalProgress++;
                     CoalProgress++;
-                    Debug.Log("Extra Steam Coal Progress X3");
+                    Debug.Log("Extra Steam Coal Progress X3");                                          // 3x coal progress
                 }
-                else if (CoalProgress >= 100)
+                else if (CoalProgress >= 100)                                                           // reset and add 1 coal
                 {
                     CoalProgress = 0;
                     ResourceView.CoalAmt++;
                 }
-                else if (OtherProgress >= 100)
+                else if (OtherProgress >= 100)                                                          // reset and add other
                 {
                     ResourceView.OtherMatAmt++;
                     OtherProgress = 0;
                 }
                 else
                 {
-                    OtherProgress++;
+                    OtherProgress++;                                                                    // 3x Other progress
                     OtherProgress++;
                     OtherProgress++;
                     Debug.Log("Extra Steam Other Progress X3");
                 }
             }
-            else if (Input.GetComponent<Steam>().steamheld < 2)
+            else if (Input.GetComponent<Steam>().steamheld < 2)                                         // If less than 2
             {
-                Eff = false;
-                Chance = Random.Range(0.0f, 1.0f);
+                Eff = false;                                                                            // no efficency boost
+                Chance = Random.Range(0.0f, 1.0f);                                                      // determine gathered materail
                 Input.GetComponent<Steam>().steamheld--;
-
-                if (Chance >= .5 && CoalProgress < 100)
+    
+                if (Chance >= .5 && CoalProgress < 100)                                                 // single progress coal
                 {
                     CoalProgress++;
                 }
-                else if (Chance >= .5 && CoalProgress >= 100)
+                else if (Chance >= .5 && CoalProgress >= 100)                                           // reset coal add +1 to stockpile
                 {
                     CoalProgress = 0;
                     ResourceView.CoalAmt++;
                 }
                 else if (Chance < .5 && OtherProgress >= 100)
                 {
-                    ResourceView.OtherMatAmt++;
+                    ResourceView.OtherMatAmt++;                                                         // reset other add +1
                     OtherProgress = 0;
                 }
                 else
@@ -85,10 +85,10 @@ public class Miner : MonoBehaviour
 
 
     }
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)                                                       // Attachment logic to pipes
     {
         Debug.Log("Attached");
-        if (collision.gameObject.name == "Pipe_hidden(Clone)")
+        if (collision.gameObject.name == "Pipe_hidden(Clone)")      
         {
             Debug.Log("Is Pipe");
             Input = collision.gameObject;
