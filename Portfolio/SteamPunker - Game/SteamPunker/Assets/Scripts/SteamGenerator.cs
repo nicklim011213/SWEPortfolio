18bd10130ca8a122d134d69bdfd82162d9d7c105
nicklim011213 <113318249+nicklim011213@users.noreply.github.com>
2022-09-18 21:01:52 -0400

    Update SteamGenerator.cs

--- Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/SteamGenerator.cs
+++ Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/SteamGenerator.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class SteamGenerator : MonoBehaviour
 {
-    public int steamheld;
-    public bool Connection;
-    public GameObject Output;
-    public int Fuel;
-    public Resource_Mgr ResourceView;
+    public int steamheld; // steam held before pipe output
+    public bool Connection; // is it connected
+    public GameObject Output; // the poutput object
+    public int Fuel;           // the fuel (coal)
+    public Resource_Mgr ResourceView; // the resource view (so it can reduce coal in view)
     // Start is called before the first frame update
     void Start()
     {
         steamheld = 0;
         Connection = false;
         Fuel = 250;
-        ResourceView = GameObject.Find("Canvas").transform.GetChild(2).GetComponent<Resource_Mgr>();
+        ResourceView = GameObject.Find("Canvas").transform.GetChild(2).GetComponent<Resource_Mgr>(); // This is the resource screen
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision) // When a collision with another object is detected
     {
-        Output = collision.gameObject;
+        Output = collision.gameObject;          // its an output
         Connection = true;
-        if (Output != null)
+        if (Output != null)                     // if nothing there then no pipe otherwise there is a pipe connection
         {
-            Debug.Log("Found Pipe");
+            Debug.Log("Found Pipe");          
         }
         else
         {
@@ -35,20 +35,20 @@ public class SteamGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (steamheld < 0)
+        if (steamheld < 0)      // Fixes a small bug
         {
             steamheld = 0;
         }
-        if (Connection && Fuel > 0)
+        if (Connection && Fuel > 0) // if it is running with a connection
         {
-            steamheld++;
-            Fuel--;  
+            steamheld++; // make steam
+            Fuel--;  // reduce fuel
         }
-        else if (Connection && Fuel == 0 && ResourceView.CoalAmt > 0)
+        else if (Connection && Fuel == 0 && ResourceView.CoalAmt > 0) // if no fuel and you have coal in your resources
         {
-            ResourceView.CoalAmt--;
-            steamheld++;
-            Fuel = 250;
+            ResourceView.CoalAmt--; // reduce coal amount in resources view
+            steamheld++;            // make a steam
+            Fuel = 250;             // add 250 fuel
         }
         else
         {
