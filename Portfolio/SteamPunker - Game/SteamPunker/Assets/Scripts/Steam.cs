9e98fbd38038f85fc472e0b0b85a5bf1bdc06ef4
nicklim011213 <113318249+nicklim011213@users.noreply.github.com>
2022-09-18 20:58:21 -0400

    Update Steam.cs

--- Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Steam.cs
+++ Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Steam.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Steam: MonoBehaviour
 {
-    public int steamheld = 0;
-    public GameObject Output;
+    public int steamheld = 0; // number of steam held
+    public GameObject Output; // input and output pipes
     public GameObject Input;
-    public Steam Inputscript;
+    public Steam Inputscript; // the scripts which handle input
     public SteamGenerator Inputscript2;
     // Start is called before the first frame update
     void Start()
     {
-        Input = null;
+        Input = null; // Input is nothing to start
     }
 
     // Update is called once per frame
@@ -27,7 +27,7 @@ public class Steam: MonoBehaviour
                     steamheld++;
                     steamheld++;
                 }
-                else
+                else // Otherwise drain at 1 per second
                 {
                     Inputscript.steamheld--;
                     steamheld++;
@@ -43,7 +43,7 @@ public class Steam: MonoBehaviour
                 steamheld++;
                 steamheld++;
             }
-            else
+            else                        // otherwise base speed
             {
                 Inputscript2.steamheld--;
                 steamheld++;
@@ -52,19 +52,20 @@ public class Steam: MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if ((collision.gameObject.name == "Pipe_hidden(Clone)" && collision.gameObject.GetComponent<Steam>().steamheld > this.steamheld))
+        if ((collision.gameObject.name == "Pipe_hidden(Clone)" && collision.gameObject.GetComponent<Steam>().steamheld > this.steamheld)) // If a pipe is found and is colliding
         {
-            Debug.Log("Found pipe with more than 0 steam Assuming Input");
-            Input = collision.gameObject;
-            Inputscript = Input.GetComponent<Steam>();
+            Debug.Log("Found pipe with more than 0 steam Assuming Input"); // if pipe has more than 0 safe to assume this is an input pipe
+            Input = collision.gameObject;                                   // make the pipe input
+            Inputscript = Input.GetComponent<Steam>();                     // input script is the objects script
         }
         else if ((collision.gameObject.name == "SteamGen_hidden(Clone)"))
         {
-            Input = collision.gameObject;
-            Inputscript2 = Input.GetComponent<SteamGenerator>();
+            Input = collision.gameObject;                               // If generator of steam
+            Inputscript2 = Input.GetComponent<SteamGenerator>();        // set input 2 = to that script the steam gen has
         }
         if ((collision.gameObject.name == "Pipe_hidden(Clone)" && collision.gameObject.GetComponent<Steam>().steamheld == 0) || (collision.gameObject.name == "Miner_hidden(Clone)"))
         {
+           // If pipe has 0 steam safe to assume output
            Debug.Log("Found pipe with 0 steam Assuming output");
            Output = collision.gameObject;
         }
