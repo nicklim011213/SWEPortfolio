c1e5a219ad335f426a5579614a2eb1ec73e31b97
nicklim011213 <113318249+nicklim011213@users.noreply.github.com>
2022-09-18 20:49:53 -0400

    Update Resource_Mgr.cs

--- Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Resource_Mgr.cs
+++ Portfolio/SteamPunker - Game/SteamPunker/Assets/Scripts/Resource_Mgr.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class Resource_Mgr : MonoBehaviour
 {
-    public int CoalAmt;
-    public int OtherMatAmt;
+    public int CoalAmt;         // Int stores amount of coal
+    public int OtherMatAmt;     // Int stores amount of other
     // Start is called before the first frame update
     void Start()
     {
-        CoalAmt = 0;
+        CoalAmt = 0;           // Set to 0 when start
         OtherMatAmt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Text>().text = "Resources:\nCoal: " + CoalAmt  +"\nOther: " + OtherMatAmt;
+        this.GetComponent<Text>().text = "Resources:\nCoal: " + CoalAmt  +"\nOther: " + OtherMatAmt;    // Display it on screen. Resource gathers update this value
     }
 }
